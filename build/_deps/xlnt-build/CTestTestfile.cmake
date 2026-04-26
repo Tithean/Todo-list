a618a11c2702a0c4ada5b8aa355ea8d21f43a7d2
titheanistad <htitheanistad@gmail.com>
2026-04-26 12:10:37 +0700

    remove build from repo

--- build/_deps/xlnt-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/class/istad/cpp-final-project/todo-list/build/_deps/xlnt-src
-# Build directory: D:/class/istad/cpp-final-project/todo-list/build/_deps/xlnt-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("source")
--- build/_deps/xlnt-build/source/third-party/libstudxml/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/class/istad/cpp-final-project/todo-list/build/_deps/xlnt-src/third-party/libstudxml.build
-# Build directory: D:/class/istad/cpp-final-project/todo-list/build/_deps/xlnt-build/source/third-party/libstudxml
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
