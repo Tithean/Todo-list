28fa0754fea04a37cfe1965be39fcc5c7dcb988a
titheanistad <htitheanistad@gmail.com>
2026-04-26 12:03:03 +0700

    first commit

---
+++ build/_deps/xlnt-build/source/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: D:/class/istad/cpp-final-project/todo-list/build/_deps/xlnt-src/source
+# Build directory: D:/class/istad/cpp-final-project/todo-list/build/_deps/xlnt-build/source
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("third-party/libstudxml")
